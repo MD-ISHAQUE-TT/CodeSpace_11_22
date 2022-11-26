db5290cb24db1e82a5bc596fd4604d17a286be76
MD-ISHAQUE-TT <56503630+MD-ISHAQUE-TT@users.noreply.github.com>
2022-11-26 15:31:53 +0000

    first home route created

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/home", (req, res) => {
+    res.send("<h1>You are visiting home page</h1>")
+})
+
+module.exports =app;
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log(`Hello there, Welcome to github codespace w`);
+const app = require("./app");
+require('dotenv').config()
+// const {PORT} = 4000 || process.env.PORT;
+const {PORT} = process.env.PORT;
+app.listen(PORT,() => {
+    console.log(`Server is listening at port ${PORT}`);
+})
