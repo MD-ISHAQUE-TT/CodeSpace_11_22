197368f2cb8fc3a9a6428cd0eba7273256adc157
MD-ISHAQUE-TT <56503630+MD-ISHAQUE-TT@users.noreply.github.com>
2022-11-26 15:53:50 +0000

    route issue

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 require('dotenv').config()
+
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("<h1>You are visiting home page</h1>")
+    res.send("<h1>Server is working</h1>")
 })
 
 module.exports =app;
