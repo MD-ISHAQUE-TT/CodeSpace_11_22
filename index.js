ac5d42013c05c90792e411fc30c7ccb3c8847328
MD-ISHAQUE-TT <56503630+MD-ISHAQUE-TT@users.noreply.github.com>
2022-11-26 15:44:35 +0000

    used interpolation for port undefined issue

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const app = require("./app");
 
 // const {PORT} = 4000 || process.env.PORT;
-const PORT = process.env.PORT;
+// const PORT = process.env.PORT;
+const {PORT} = process.env; // This and above declaration are same
 app.listen(PORT,() => {
     console.log(`Server is listening at port ${PORT}`);
 })
